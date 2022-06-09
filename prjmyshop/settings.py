6d7b2d22f3418394b60ef852ef849b2150d99657
vishnu pv <vishnure7720@gmail.com>
2022-06-09 14:01:07 +0530

    settings changed

--- prjmyshop/settings.py
+++ prjmyshop/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'prjmyshop/static/')
+   os.path.join(BASE_DIR, 'myshop/static/')
 ]
 
 # Default primary key field type
