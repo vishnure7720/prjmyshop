457db688dbd94db6bb6821105ff56c41ce717880
vishnu pv <vishnure7720@gmail.com>
2022-06-09 14:14:06 +0530

    update commit

--- prjmyshop/settings.py
+++ prjmyshop/settings.py
@@ -42,9 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-      'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
-  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
